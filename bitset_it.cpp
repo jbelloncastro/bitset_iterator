2ce113ef114417da33905d9eec4a5534b5f5e77b
Jorge <jbelloncastro@users.noreply.github.com>
2019-06-15 11:12:23 +0100

    Update examples

--- bitset_it.cpp
+++ bitset_it.cpp
@@ -5,8 +5,15 @@
 #include <iostream>
 
 int main() {
-    std::bitset<64> a(333);
+    std::bitset<10> a{333};
+    
+    // Iterate over all values
     for( auto v : a ) {
     	std::cout << v << " "; // prints 0 2 3 6 8
     }
+    
+    // Num bits set (works but inefficient)
+    auto num_set = std::distance( std::begin(a), std::end(a) );
+    
+    return 0;
 }
